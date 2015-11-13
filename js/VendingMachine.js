62bf42b20bfe35f9cc8bf2a72e8fcc6103a0eb44
John Ryan <john.ryan@cardinalhealth.com>
2015-11-13 14:36:49 -0500

    Added the insert coin logic.

--- js/VendingMachine.js
+++ js/VendingMachine.js
@@ -1,9 +1,31 @@
-var vendingMachine = {
+var johnryanKata = johnryanKata || {};
+johnryanKata.vendingMachine = {
   create: function() {
     var self = {};
+    var currentAmount = 0;
 
     self.display = function() {
-      return "INSERT COIN";
+      return currentAmount === 0 ? "INSERT COIN" : amountFormatter(
+        currentAmount);
+    };
+
+    function amountFormatter(amount) {
+      return "$" + (amount / 100).toFixed(2);
+    }
+
+    self.insert = function(coin) {
+      switch (coin) {
+        case "NICKEL":
+          currentAmount += 5;
+          break;
+        case "DIME":
+          currentAmount += 10;
+          break;
+        case "QUARTER":
+          currentAmount += 25;
+          break;
+        default:
+      }
     };
 
     return self;
--- spec/VendingMachine.spec.js
+++ spec/VendingMachine.spec.js
@@ -1,16 +1,71 @@
 describe("Vending Machine", function() {
-
   var subject;
 
   beforeEach(function() {
-    subject = vendingMachine.create();
+    subject = johnryanKata.vendingMachine.create();
   });
 
-  when("When there are no coins inserted", function() {
+  it("displays INSERT COIN", function() {
+    expect(subject.display()).toEqual("INSERT COIN");
+  });
+
+  when("A nickel is inserted", function() {
+    beforeEach(function() {
+      subject.insert("NICKEL");
+    });
+
+    it("will display '$0.05'", function() {
+      expect(subject.display()).toEqual("$0.05");
+    });
+  });
+
+  when("A dime is inserted", function() {
+    beforeEach(function() {
+      subject.insert("DIME");
+    });
+
+    it("will display '$0.10'", function() {
+      expect(subject.display()).toEqual("$0.10");
+    });
+
+    when("A quarter is inserted", function() {
+      beforeEach(function() {
+        subject.insert("QUARTER");
+      });
+
+      it("will display '$0.35'", function() {
+        expect(subject.display()).toEqual("$0.35");
+      });
+    });
+  });
+
+  when("A quarter is inserted", function() {
+    beforeEach(function() {
+      subject.insert("QUARTER");
+    });
+
+    it("will display '$0.25'", function() {
+      expect(subject.display()).toEqual("$0.25");
+    });
+
+    when("An invalid coin is inserted", function() {
+      beforeEach(function() {
+        subject.insert("WOODEN NICKEL");
+      });
+
+      it("will display '$0.25'", function() {
+        expect(subject.display()).toEqual("$0.25");
+      });
+    });
+  });
+
+  when("An invalid coin is inserted", function() {
+    beforeEach(function() {
+      subject.insert("WOODEN NICKEL");
+    });
 
     it("displays INSERT COIN", function() {
       expect(subject.display()).toEqual("INSERT COIN");
     });
   });
-
 });
